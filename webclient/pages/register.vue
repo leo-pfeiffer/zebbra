aa71f09082c803599eb4c8d90feef8147cfb2892
pschnyder <pat.schnyder96@gmail.com>
2022-06-14 17:18:11 +0100

    added register page

---
+++ webclient/pages/register.vue
@@ -0,0 +1,8 @@
+<template>
+  <div class="container flex justify-center">
+    <div class="basis-1/4">
+      <h1 class="text-2xl my-4 text-zinc-900 font-semibold">Register</h1>
+    </div>
+  
+  </div>
+</template>
