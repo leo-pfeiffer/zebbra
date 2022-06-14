e25ca74f45c361edb5f6d7b7791debed0dff8b40
leo-pfeiffer <leopold.pfeiffer@icloud.com>
2022-06-14 14:55:28 +0100

    test: database connection established

--- server/tests/test_database.py
+++ server/tests/test_database.py
@@ -6,3 +6,8 @@ def test_settings_env_is_test():
     settings = get_settings()
     assert settings.dict()['ENV'] == 'test'
     assert settings.dict()['MONGODB_DB'] == 'zebbra_test'
+
+
+def test_database_is_connected():
+    from core.models.database import db
+    assert db.name == 'zebbra_test'
