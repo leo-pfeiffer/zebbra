355b536fe25feedcc1f3ca47885d8b8335ae57f2
pschnyder <pat.schnyder96@gmail.com>
2022-06-15 16:09:01 +0100

    added button to create new model to nav

--- webclient/layouts/navbar.vue
+++ webclient/layouts/navbar.vue
@@ -3,7 +3,8 @@ export default {
   data() {
     return {
       workspaceName: "",
-      models: []
+      models: [],
+      visible: true
     }
   },
   beforeMount() {
@@ -15,7 +16,7 @@ export default {
 
 <template>
     <div class="w-screen h-screen flex">
-        <div class="w-48 bg-zinc-100 px-3 py-6">
+        <div class="w-48 bg-zinc-100 px-3 py-6 border-r border-zinc-300">
             <div>
                 <span class="px-2 py-1 rounded-md bg-green-500 text-neutral-50">
                     {{ this.workspaceName[0] }}
@@ -24,7 +25,7 @@ export default {
                     {{ this.workspaceName }}
                 </span>
             </div>
-            <div class="text-sm mt-4">
+            <div class="text-sm my-5">
                 <div class="my-1.5 pl-1.5">
                     <NuxtLink to="/settings"><i class="bi bi-gear-fill text-zinc-400"></i><span class="pl-3.5 text-zinc-500 hover:text-zinc-600">Settings</span></NuxtLink>
                 </div>
@@ -35,10 +36,22 @@ export default {
                     <NuxtLink to="/documentation"><i class="bi bi-file-earmark-code-fill text-zinc-400"></i><span class="pl-3.5 text-zinc-500 hover:text-zinc-600">Documentation</span></NuxtLink>
                 </div>
             </div>
-            <div class="my-6">
+            <div class="my-6 flex justify-center w-full">
 
-                <button class=""><i class="bi bi-plus-lg"></i>Create New Model</button>
+                <button class=" bg-zinc-50 hover:bg-zinc-100 drop-shadow-sm shadow-inner shadow-zinc-50 font-medium text-xs px-2 py-1 border border-zinc-300 rounded-md text-zinc-700"><i class="bi bi-plus-lg mr-1"></i>Create New Model</button>
 
+            </div>
+            <div>
+                <div>
+                    <span class="text-xs text-zinc-500">YOUR MODELS</span>
+                    <div v-if="false">
+                        <span>Model 1</span>
+                    </div>
+                    <div v-else>
+                        <span>No models yet</span>
+
+                    </div>
+                </div>
             </div>
         </div>
         <div class="flex-grow">
