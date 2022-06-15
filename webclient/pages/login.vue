a304bf5b5979bd60b54487e7f2f828c71ecbedfa
pschnyder <pat.schnyder96@gmail.com>
2022-06-15 09:58:14 +0100

    login page beatuifications

--- webclient/pages/login.vue
+++ webclient/pages/login.vue
@@ -1,26 +1,35 @@
 <template>
   <div class="container flex justify-center mx-auto">
-    <div class="">
-      <h1 class="text-2xl my-4 text-zinc-900 font-semibold">Login</h1>
-      <form>
-        <div>
-          <label class="block text-xs font-medium text-zinc-500" for="login-email">Email</label>
-          <div class="mt-1">
-            <input required class="border-zinc-300 border rounded text-sm focus:ring-sky-500 focus:border-sky-500 px-2.5 py-1 placeholder:text-zinc-400" id="login-email" type="email" placeholder="you@company.com">
+    <div class="w-1/2 md:w-1/4">
+      <div>
+        <h1 class="text-2xl mb-4 mt-16 text-zinc-900 font-semibold">Login</h1>
+      </div>
+      <div class="w-full">
+        <form>
+          <div>
+            <label class="block text-xs font-medium text-zinc-500" for="login-email">Email</label>
+            <div class="mt-1">
+              <input required
+                class="w-full border-zinc-300 border rounded text-sm focus:ring-sky-500 focus:border-sky-500 px-2.5 py-1 placeholder:text-zinc-400"
+                id="login-email" type="email" placeholder="you@company.com">
+            </div>
           </div>
-        </div>
-        <div class="mt-2">
-          <label class="block text-xs font-medium text-zinc-500" for="login-password">Password</label>
-          <div class="mt-1">
-            <input required class="border-zinc-300 border rounded text-sm focus:ring-sky-500 focus:border-sky-500 px-2.5 py-1 placeholder:text-zinc-400" id="login-password" type="password" placeholder="Password">
+          <div class="mt-2">
+            <label class="block text-xs font-medium text-zinc-500" for="login-password">Password</label>
+            <div class="mt-1">
+              <input required
+                class="w-full border-zinc-300 border rounded text-sm focus:ring-sky-500 focus:border-sky-500 px-2.5 py-1 placeholder:text-zinc-400"
+                id="login-password" type="password" placeholder="Password">
+            </div>
           </div>
-        </div>
-        <div class="mt-3">
-          <button type="submit" class="bg-zinc-50 hover:bg-zinc-100 drop-shadow-sm shadow-inner shadow-zinc-50 font-medium text-sm px-2.5 py-1 border border-zinc-300 rounded text-zinc-700">Login</button>
-          <NuxtLink to="/register"><small class="ml-2 text-green-500 hover:text-green-700">or register here.</small></NuxtLink>
-        </div>
-      </form>
+          <div class="mt-3">
+            <button type="submit"
+              class="bg-zinc-50 hover:bg-zinc-100 drop-shadow-sm shadow-inner shadow-zinc-50 font-medium text-sm px-2.5 py-1 border border-zinc-300 rounded text-zinc-700">Login</button>
+            <NuxtLink to="/register"><small class="ml-2 text-green-500 hover:text-green-700">or register here.</small>
+            </NuxtLink>
+          </div>
+        </form>
+      </div>
     </div>
-  
   </div>
 </template>
