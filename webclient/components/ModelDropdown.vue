0f1949d98272362f2affe42477414a1989da679d
pschnyder <pat.schnyder96@gmail.com>
2022-06-15 18:25:02 +0100

    added model section to navbar

---
+++ webclient/components/ModelDropdown.vue
@@ -0,0 +1,41 @@
+<template>
+    <div class="my-2 text-zinc-500">
+        <div @click="clickDropdown" class="hover:cursor-pointer flex align-middle">
+            <i v-if="!opened" class="bi bi-caret-right-fill mr-2 text-sm"></i>
+            <i v-else class="bi bi-caret-down-fill mr-2 text-sm"></i>
+            {{ model.name }}
+        </div>
+        <div v-show="opened" class="text-sm ml-10 mt-1">
+            <ul class="list-disc">
+                <!-- todo: update links, add active state and default open for selected model -->
+                <NuxtLink to="/tesla/{{ $model.id }}/dashboard"><li class="my-1">Dashboard</li></NuxtLink>
+                <NuxtLink to="/tesla/{{ $model.id }}/profit-loss"><li class="my-1">Profit & Loss</li></NuxtLink>
+                <NuxtLink to="/tesla/{{ $model.id }}/Revenues"><li class="my-1">Revenues</li></NuxtLink>
+                <NuxtLink to="/tesla/{{ $model.id }}/costs"><li class="my-1 ">Costs</li></NuxtLink>
+            </ul>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            opened: false
+        }
+    },
+    props: {
+        model: Object
+    },
+    methods: {
+        clickDropdown() {
+            if (this.opened === true) {
+                this.opened = false;
+            } else {
+                this.opened = true;
+            }
+        }
+    },
+}
+
+</script>
--- webclient/layouts/navbar.vue
+++ webclient/layouts/navbar.vue
@@ -1,55 +1,60 @@
 <script>
 export default {
-  data() {
-    return {
-      workspaceName: "",
-      models: [],
-      visible: true
+    data() {
+        return {
+            workspaceName: 'Tesla',
+            items: [
+                { name: 'Model S', id: "1" },
+                { name: 'Model 3', id: "2" },
+                { name: 'Model X', id: "3" }
+            ]
     }
-  },
-  beforeMount() {
-    console.log("beforemount")
-    this.workspaceName = "Acme"
-  }
+}
 }
 </script>
 
 <template>
     <div class="w-screen h-screen flex">
-        <div class="w-48 bg-zinc-100 px-3 py-6 border-r border-zinc-300">
+        <div class="w-48 bg-zinc-100 px-3 py-4 border-r border-zinc-300 overflow-hidden">
             <div>
-                <span class="px-2 py-1 rounded-md bg-green-500 text-neutral-50">
-                    {{ this.workspaceName[0] }}
+                <span class="px-2 py-1 rounded-md bg-green-500 text-neutral-50 shadow-sm">
+                    {{ workspaceName[0] }}
                 </span>
                 <span class="ml-2 text-zinc-900">
-                    {{ this.workspaceName }}
+                    {{ workspaceName }}
                 </span>
             </div>
             <div class="text-sm my-5">
                 <div class="my-1.5 pl-1.5">
-                    <NuxtLink to="/settings"><i class="bi bi-gear-fill text-zinc-400"></i><span class="pl-3.5 text-zinc-500 hover:text-zinc-600">Settings</span></NuxtLink>
+                    <NuxtLink to="/settings"><i class="bi bi-gear-fill text-zinc-400"></i><span
+                            class="pl-3.5 text-zinc-500 hover:text-zinc-600">Settings</span></NuxtLink>
                 </div>
                 <div class="my-1.5 pl-1.5">
-                    <NuxtLink to="/integrations"><i class="bi bi-cloud-fill text-zinc-400"></i><span class="pl-3.5 text-zinc-500 hover:text-zinc-600">Integrations</span></NuxtLink>
+                    <NuxtLink to="/integrations"><i class="bi bi-cloud-fill text-zinc-400"></i><span
+                            class="pl-3.5 text-zinc-500 hover:text-zinc-600">Integrations</span></NuxtLink>
                 </div>
                 <div class="my-1.5 pl-1.5">
-                    <NuxtLink to="/documentation"><i class="bi bi-file-earmark-code-fill text-zinc-400"></i><span class="pl-3.5 text-zinc-500 hover:text-zinc-600">Documentation</span></NuxtLink>
+                    <NuxtLink to="/documentation"><i class="bi bi-file-earmark-code-fill text-zinc-400"></i><span
+                            class="pl-3.5 text-zinc-500 hover:text-zinc-600">Documentation</span></NuxtLink>
                 </div>
             </div>
             <div class="my-6 flex justify-center w-full">
 
-                <button class=" bg-zinc-50 hover:bg-zinc-100 drop-shadow-sm shadow-inner shadow-zinc-50 font-medium text-xs px-2 py-1 border border-zinc-300 rounded-md text-zinc-700"><i class="bi bi-plus-lg mr-1"></i>Create New Model</button>
+                <button
+                    class=" bg-zinc-50 hover:bg-zinc-100 drop-shadow-sm shadow-inner shadow-zinc-50 font-medium text-xs px-2 py-1 border border-zinc-300 rounded-md text-zinc-700"><i
+                        class="bi bi-plus-lg mr-1"></i>Create New Model</button>
 
             </div>
-            <div>
+            <div class="border-b border-zinc-300 pb-3">
                 <div>
                     <span class="text-xs text-zinc-500">YOUR MODELS</span>
-                    <div v-if="false">
-                        <span>Model 1</span>
+                    <div v-if="true" class="overflow-auto min-h-[50vh] max-h-[50vh]">
+                        <div v-for="item in items">
+                            <ModelDropdown :model=item></ModelDropdown>
+                        </div>
                     </div>
                     <div v-else>
-                        <span>No models yet</span>
-
+                        <p class="text-xs mt-2 text-zinc-500">Start by creating your first model.</p>
                     </div>
                 </div>
             </div>
