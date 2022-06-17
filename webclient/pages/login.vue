4946f5ea26e07bade1c5a01bf98412dfb18082ea
pschnyder <pat.schnyder96@gmail.com>
2022-06-17 11:49:26 +0100

    created component for error messages

--- webclient/pages/login.vue
+++ webclient/pages/login.vue
@@ -30,7 +30,7 @@
           </div>
         </form>
         <div v-show="showError" class="w-full flex justify-center">
-          <div class="w-max static mt-5 py-2 px-3 border border-red-300 rounded shadow-sm shadow-red-100 text-xs text-red-500 text-center" ><i class="bi bi-exclamation-triangle-fill mr-2"></i>{{ errorMessage }} </div>
+          <ErrorMessage :error-message="errorMessage"></ErrorMessage>
         </div>
       </div>
     </div>
@@ -46,7 +46,7 @@ export default {
         password: ""
       },
       showError: false, 
-      errorMessage: "incorrect password"
+      errorMessage: "Login failed. Try again!"
     };
   },
   methods: {
--- webclient/pages/register.vue
+++ webclient/pages/register.vue
@@ -55,7 +55,7 @@
           </div>
         </form>
         <div v-show="showError" class="w-full flex justify-center">
-          <div class="w-max static mt-5 py-2 px-3 border border-red-300 rounded shadow-sm shadow-red-100 text-xs text-red-500 text-center" ><i class="bi bi-exclamation-triangle-fill mr-2"></i>{{ errorMessage }} </div>
+          <ErrorMessage :error-message="errorMessage"></ErrorMessage>
         </div>
       </div>
     </div>
@@ -76,7 +76,7 @@ export default {
       },
       workspaceInput: "",
       showError: false,
-      errorMessage: ""
+      errorMessage: "Registration failed. Try again!"  
     };
   },
   methods: {
