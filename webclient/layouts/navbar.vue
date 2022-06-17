c22a01efdcf55ece885d3ce98b0b92604b91d25f
pschnyder <pat.schnyder96@gmail.com>
2022-06-17 14:59:32 +0100

    improved navbar design

--- webclient/layouts/navbar.vue
+++ webclient/layouts/navbar.vue
@@ -16,7 +16,7 @@ export default {
 <template>
     <div class="w-screen h-screen flex">
         <div class="w-48 bg-zinc-100 px-3 py-4 border-r border-zinc-300 overflow-hidden">
-            <div>
+            <div class="text-sm ml-0.5">
                 <span class="px-2 py-1 rounded-md bg-green-500 text-neutral-50 shadow-sm">
                     {{ workspaceName[0] }}
                 </span>
@@ -24,30 +24,30 @@ export default {
                     {{ workspaceName }}
                 </span>
             </div>
-            <div class="text-sm my-5">
-                <div class="my-1.5 pl-1.5">
-                    <NuxtLink to="/settings"><i class="bi bi-gear-fill text-zinc-400"></i><span
-                            class="pl-3.5 text-zinc-500 hover:text-zinc-600">Settings</span></NuxtLink>
-                </div>
-                <div class="my-1.5 pl-1.5">
-                    <NuxtLink to="/integrations"><i class="bi bi-cloud-fill text-zinc-400"></i><span
-                            class="pl-3.5 text-zinc-500 hover:text-zinc-600">Integrations</span></NuxtLink>
-                </div>
-                <div class="my-1.5 pl-1.5">
-                    <NuxtLink to="/documentation"><i class="bi bi-file-earmark-code-fill text-zinc-400"></i><span
-                            class="pl-3.5 text-zinc-500 hover:text-zinc-600">Documentation</span></NuxtLink>
-                </div>
+            <div class="text-xs my-4">
+                <NuxtLink to="/settings"><div class="px-2 hover:bg-zinc-200 py-1.5 rounded text-zinc-500">
+                    <i class="bi bi-gear-fill text-zinc-400"></i><span
+                            class="pl-2">Settings</span>
+                </div></NuxtLink>
+                <NuxtLink to="/settings"><div class="px-2 hover:bg-zinc-200 py-1.5 rounded text-zinc-500">
+                    <i class="bi bi-cloud-fill text-zinc-400"></i><span
+                            class="pl-2">Integrations</span>
+                </div></NuxtLink>
+                <NuxtLink to="/settings"><div class="px-2 hover:bg-zinc-200 py-1.5 rounded text-zinc-500">
+                    <i class="bi bi-file-earmark-code-fill text-zinc-400"></i><span
+                            class="pl-2">Documentation</span>
+                </div></NuxtLink>
             </div>
-            <div class="my-6 flex justify-center w-full">
+            <div class="my-4 flex justify-center w-full">
 
                 <button
-                    class=" bg-zinc-50 hover:bg-zinc-100 drop-shadow-sm shadow-inner shadow-zinc-50 font-medium text-xs px-2 py-1 border border-zinc-300 rounded text-zinc-700"><i
-                        class="bi bi-plus-lg mr-1"></i>Create New Model</button>
+                    class=" bg-zinc-50 hover:bg-zinc-100 drop-shadow-sm shadow-inner shadow-zinc-50 font-medium text-xs pl-2.5 pr-3 py-1 border border-zinc-300 rounded text-zinc-700"><i
+                        class="bi bi-bar-chart-fill mr-1"></i>Create New Model</button>
 
             </div>
             <div class="pb-3">
                 <div>
-                    <span class="text-xs text-zinc-500">YOUR MODELS</span>
+                    <span class="text-xs text-zinc-500">Your Models</span>
                     <div v-if="true" class="overflow-auto min-h-[50vh] max-h-[50vh]">
                         <div v-for="model in models">
                             <ModelDropdown :model=model></ModelDropdown>
