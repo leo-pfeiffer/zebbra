8ddd1f745fa97a428fa6d4639e2df6efd1d43e71
leo-pfeiffer <leopold.pfeiffer@icloud.com>
2022-06-18 12:32:56 +0100

    fix: CI

--- server/scripts/encrypt_env.sh
+++ server/scripts/encrypt_env.sh
@@ -1,4 +1,4 @@
-#!/bin/zsh
+#!/bin/bash
 source .env
 
 openssl enc -aes-256-cbc -e -in ".env"  -out "encrypted_dotenv" -pass pass:$ENV_ENCRYPT_PASS
--- server/scripts/gh_set_secret.sh
+++ server/scripts/gh_set_secret.sh
@@ -1,4 +1,4 @@
-#!/bin/zsh
+#!/bin/bash
 source .env
 
 echo "$ENV_ENCRYPT_PASS"
