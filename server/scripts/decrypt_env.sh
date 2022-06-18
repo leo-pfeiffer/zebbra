ff80bb4056fdf37c8138b36382b0092307bad763
leo-pfeiffer <leopold.pfeiffer@icloud.com>
2022-06-18 15:09:11 +0100

    fix: ci

--- server/scripts/decrypt_env.sh
+++ server/scripts/decrypt_env.sh
@@ -1,4 +1,4 @@
 #!/bin/zsh
 
 cat .env.enc | \
-  openssl enc -base64 -d -aes-256-cbc -nosalt -pass pass:$ENV_ENCRYPT_PASS
+  openssl enc -base64 -d -aes-256-cbc -nosalt -pass pass:$ENV_ENCRYPT_PASS > .env
