8a118beef1df91cb75943dc87bdb7a4a1a6a542c
pschnyder <pat.schnyder96@gmail.com>
2022-06-28 18:34:36 +0100

    created composable that returns if user is admin or not

---
+++ webclient/composables/useIsAdmin.ts
@@ -0,0 +1,45 @@
+import { useFetchAuth } from "./useFetchAuth"
+
+export const useIsAdmin = async () => {
+
+    var userUsername: String;
+    var adminUsername: String;
+
+    type GetUserResponse = {
+        first_name: String;
+        last_name: String;
+        username: String;
+        password: String;
+    }
+
+    type GetWorkspaceResponse = {
+        name: String;
+        admin: String;
+        users: String[];
+    }
+
+    //get the username and store it
+    const getUserUsername = await useFetchAuth(
+        'http://localhost:8000/user',{ method: 'GET'}
+        ).then((data:GetUserResponse) => {
+          console.log(data);
+          userUsername = data.username;
+
+        }).catch((error) => {
+          console.log(error);
+          });
+    
+    //get the admins username and store it
+    const getAdminUsername = await useFetchAuth(
+    'http://localhost:8000/workspace',{ method: 'GET'}
+    ).then((data:GetWorkspaceResponse) => {
+        console.log(data);
+        adminUsername = data[0].admin;
+    }).catch((error) => {
+        console.log(error);
+        });
+
+    //return if equals
+
+    return (adminUsername === userUsername);
+}
