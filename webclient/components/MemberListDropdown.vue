3f5a17e7ba19731cc73cf66062b89fbce84a0461
pschnyder <pat.schnyder96@gmail.com>
2022-06-29 10:05:56 +0100

    design improvement in member list toggle

--- webclient/components/MemberListDropdown.vue
+++ webclient/components/MemberListDropdown.vue
@@ -3,22 +3,21 @@
     <div v-show="clicked"
         class="absolute z-10 -translate-x-full bg-white border border-zinc-300 shadow-sm rounded text-sm w-max">
         <div class="text-zinc-500 p-3 border-b border-zinc-300"><button @click="toggleChangeUserTypeModal"><i
-                    class="bi bi-person-lines-fill mr-1"></i>Change role</button></div>
-        <div class="text-zinc-500 p-3"><button @click="toggleDeleteUserModal"><i class="bi bi-trash mr-1"></i> Delete
-                user</button></div>
+                    class="bi bi-layers-fill mr-1.5 text-zinc-400"></i>Change role</button></div>
+        <div class="text-zinc-500 p-3"><button @click="toggleDeleteUserModal"><i class="bi bi-person-dash-fill mr-1.5 text-zinc-400"></i>Remove user</button></div>
     </div>
     <div v-show="clicked" @click="toggleDots" class="absolute top-0 left-0 w-[100vw] h-[100vh] z-0"></div>
 
     <div v-show="deleteUserModalOpen" class="absolute -translate-x-full w-max h-max flex justify-center align-middle">
         <div class="p-3 border h-max shadow-sm bg-white border-zinc-300 rounded">
-            <p class="text-zinc-900 text-sm mb-3">Are you sure you want to delete <span class="font-semibold">{{ username }}'s</span> account?</p>
+            <p class="text-zinc-500 text-sm mb-3">Are you sure you want to remove <span class="font-semibold">{{ username }}'s</span> account?</p>
             <div class="float-right">
                 <button
                     class="bg-zinc-50 hover:bg-zinc-100 drop-shadow-sm shadow-inner shadow-zinc-50 font-medium text-xs px-2 py-1 border border-zinc-300 rounded text-zinc-700"
                     @click="toggleDeleteUserModal">Cancel</button>
                 <button class="ml-2 bg-red-600  drop-shadow-sm
                     shadow-zinc-50 text-xs font-medium px-2 py-1 
-                    border border-red-600 rounded text-neutral-100" @click="deleteUser">Delete</button>
+                    border border-red-600 rounded text-neutral-100" @click="deleteUser">Remove</button>
             </div>
         </div>
     </div>
@@ -27,7 +26,7 @@
         <div class="p-3 border h-max shadow-sm bg-white border-zinc-300 rounded">
             <form @submit.prevent="updateUserType">
                 <div class="flex align-middle mb-3">
-                    <span class="text-zinc-900 mr-2 mt-1">Change <span class="font-semibold">{{ username }}'s</span> account to:</span>
+                    <span class="text-zinc-500 mr-2 mt-1">Change <span class="font-semibold">{{ username }}'s</span> account to:</span>
                     <select
                     class="text-zinc-700 border shadow-sm bg-white border-zinc-300 rounded text-sm py-1 px-1" 
                     v-model="this.selectedUserType"
