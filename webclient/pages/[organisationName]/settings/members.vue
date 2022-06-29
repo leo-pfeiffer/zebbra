bb824ab6005915928b616569db384e82a92f343d
pschnyder <pat.schnyder96@gmail.com>
2022-06-29 14:14:19 +0100

    added MemberListDropdown

--- webclient/pages/[organisationName]/settings/members.vue
+++ webclient/pages/[organisationName]/settings/members.vue
@@ -15,16 +15,22 @@ definePageMeta({
           <div class="border border-zinc-300 rounded">
             <table class="w-full text-sm text-left">
               <tr class="border-b border-zinc-300">
-                <td class="py-3 px-3 text-xs uppercase font-medium text-zinc-500 bg-zinc-100 border-zinc-300 rounded-tl">NAME</td>
+                <td
+                  class="py-3 px-3 text-xs uppercase font-medium text-zinc-500 bg-zinc-100 border-zinc-300 rounded-tl">
+                  NAME</td>
                 <td class="py-3 px-3 text-xs uppercase font-medium text-zinc-500 bg-zinc-100 border-zinc-300">EMAIL</td>
                 <td class="py-3 px-3 text-xs uppercase font-medium text-zinc-500 bg-zinc-100">ROLE</td>
                 <td class="py-3 px-3 text-xs uppercase font-medium text-zinc-500 bg-zinc-100 rounded-tr"></td>
               </tr>
               <tr v-for="member in members" class="border-b border-zinc-300 last:border-0">
-                <td class="py-3 px-3 text-zinc-900"><span class="rounded-full bg-green-500 text-neutral-50 shadow-sm text-xs p-1.5 mr-2">{{member.first_name[0]}}{{member.last_name[0]}}</span>{{member.first_name}} {{member.last_name}}</td>
-                <td class="py-3 px-3 text-zinc-500">{{member.username}}</td>
-                <td class="py-3 px-3 text-zinc-500">{{member.user_role}}</td>
-                <td class="py-3 px-3 text-zinc-500"><MemberListDropdown :username="member.username"></MemberListDropdown></td>
+                <td class="py-3 px-3 text-zinc-900"><span
+                    class="rounded-full bg-green-500 text-neutral-50 shadow-sm text-xs p-1.5 mr-2">{{ member.first_name[0] }}{{ member.last_name[0] }}</span>{{ member.first_name }}
+                  {{ member.last_name }}</td>
+                <td class="py-3 px-3 text-zinc-500">{{ member.username }}</td>
+                <td class="py-3 px-3 text-zinc-500">{{ member.user_role }}</td>
+                <td class="py-3 px-3 text-zinc-500">
+                  <MemberListDropdown :username="member.username"></MemberListDropdown>
+                </td>
               </tr>
             </table>
           </div>
@@ -67,12 +73,20 @@ export default {
     };
   },
   async beforeMount() {
+
+    const userInfo = await useUserInfo();
+    console.log(userInfo);    
+
     //get user data and pre fill the form
     /* const data = await useFetchAuth(
-      'http://localhost:8000/workspace', { method: 'GET' }
+      'http://localhost:8000/workspace/users', {
+        method: 'GET',
+      params: {
+        name: "",
+      }
+    },
     ).then((data) => {
-      console.log(data[0].users);
-      this.members = data[0].users;
+      console.log(data);
     }).catch((error) => {
       console.log(error);
       this.showError = true;
