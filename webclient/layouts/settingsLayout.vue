5b49d1d496f55d8e950c3d80a1396a43a65de9a0
pschnyder <pat.schnyder96@gmail.com>
2022-06-30 20:34:49 +0100

    refactor: updated settingsLayout to fit new data structure

--- webclient/layouts/settingsLayout.vue
+++ webclient/layouts/settingsLayout.vue
@@ -11,7 +11,7 @@ const user = useUserState();
     <div class="w-screen h-screen flex">
         <div class="w-48 min-w-[12rem] bg-zinc-100 px-3 py-4 border-r border-zinc-300 overflow-hidden">
             <div class="text-sm flex align-middle">
-                <NuxtLink :to="`/${user.workspaces[0]}`"><div class="text-zinc-400 py-1 pl-1.5 pr-1 hover:bg-zinc-200 hover:text-zinc-500 rounded-md hover:shadow-sm">
+                <NuxtLink :to="`/${user.workspaces[0].name}`"><div class="text-zinc-400 py-1 pl-1.5 pr-1 hover:bg-zinc-200 hover:text-zinc-500 rounded-md hover:shadow-sm">
                     <i class="bi bi-arrow-up-left"></i>
                 </div></NuxtLink>
                 <div class="ml-1 text-zinc-900 font-medium text-lg">Settings</div>
@@ -19,15 +19,15 @@ const user = useUserState();
             <div class="text-xs pl-2 mt-8">
                 <div class="text-zinc-500 text-semibold"><i class="bi bi-house-door-fill mr-2 text-zinc-400"></i>Workspace</div>
                 <div class="mt-1.5">
-                    <NuxtLink :to="`/${user.workspaces[0]}/settings/workspace`" activeClass="font-semibold"><div class="text-xs text-zinc-700 py-1 pl-5 hover:bg-zinc-200 rounded">General</div></NuxtLink>
-                    <NuxtLink :to="`/${user.workspaces[0]}/settings/members`" activeClass="font-semibold"><div id="workspace-members" class="text-xs text-zinc-700 py-1 pl-5 hover:bg-zinc-200 rounded">Members</div></NuxtLink>
-                    <NuxtLink :to="`/${user.workspaces[0]}/settings/integrations`" activeClass="font-semibold"><div class="text-xs text-zinc-700 py-1 pl-5 hover:bg-zinc-200 rounded">Integrations</div></NuxtLink>
+                    <NuxtLink :to="`/${user.workspaces[0].name}/settings/workspace`" activeClass="font-semibold"><div class="text-xs text-zinc-700 py-1 pl-5 hover:bg-zinc-200 rounded">General</div></NuxtLink>
+                    <NuxtLink :to="`/${user.workspaces[0].name}/settings/members`" activeClass="font-semibold"><div id="workspace-members" class="text-xs text-zinc-700 py-1 pl-5 hover:bg-zinc-200 rounded">Members</div></NuxtLink>
+                    <NuxtLink :to="`/${user.workspaces[0].name}/settings/integrations`" activeClass="font-semibold"><div class="text-xs text-zinc-700 py-1 pl-5 hover:bg-zinc-200 rounded">Integrations</div></NuxtLink>
                 </div>
             </div>
             <div class="text-xs pl-2 mt-6">
                 <div class="text-zinc-500 text-semibold"><i class="bi bi-person-fill mr-2 text-zinc-400"></i>Profile</div>
                 <div class="mt-1.5">
-                    <NuxtLink :to="`/${user.workspaces[0]}/settings/profile`" activeClass="font-semibold"><div id="profile-general" class="text-xs text-zinc-700 py-1 pl-5 hover:bg-zinc-200 rounded">General</div></NuxtLink>
+                    <NuxtLink :to="`/${user.workspaces[0].name}/settings/profile`" activeClass="font-semibold"><div id="profile-general" class="text-xs text-zinc-700 py-1 pl-5 hover:bg-zinc-200 rounded">General</div></NuxtLink>
                 </div>
             </div>
         </div>
