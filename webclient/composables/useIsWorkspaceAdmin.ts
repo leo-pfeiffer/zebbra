65733e93021e730ee87192e5e7d20b8bcf583375
pschnyder <pat.schnyder96@gmail.com>
2022-06-30 20:10:05 +0100

    updated name of isAdmin composable and deleted unnecessary console.log statements

--- webclient/composables/useIsWorkspaceAdmin.ts
+++ webclient/composables/useIsWorkspaceAdmin.ts
@@ -3,7 +3,7 @@ import { useFetchAuth } from "./useFetchAuth"
 import { GetUserResponse } from "~~/types/GetUserResponse";
 import { GetWorkspaceResponse } from "~~/types/GetWorkspaceResponse";
 
-export const useIsAdmin = async () => {
+export const useIsWorkspaceAdmin = async () => {
 
     var userUsername: String;
     var adminUsername: String;
@@ -12,7 +12,6 @@ export const useIsAdmin = async () => {
     const getUserUsername = await useFetchAuth(
         'http://localhost:8000/user',{ method: 'GET'}
         ).then((data:GetUserResponse) => {
-          console.log(data);
           userUsername = data.username;
 
         }).catch((error) => {
@@ -23,7 +22,6 @@ export const useIsAdmin = async () => {
     const getAdminUsername = await useFetchAuth(
     'http://localhost:8000/workspace',{ method: 'GET'}
     ).then((data:GetWorkspaceResponse) => {
-        console.log(data);
         adminUsername = data[0].admin;
     }).catch((error) => {
         console.log(error);
