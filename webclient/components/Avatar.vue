234c4406988e81fef8da23ad397bec8fa28e1755
pschnyder <pat.schnyder96@gmail.com>
2022-06-30 23:32:30 +0100

    loading users into member settings from api, added beautifications

---
+++ webclient/components/Avatar.vue
@@ -0,0 +1,41 @@
+<template>
+
+<span :class="getRandomColor"
+class="rounded-full tracking-tighter text-neutral-50 shadow-sm p-1.5 mr-2">{{ this.firstName[0] }}{{ this.lastName[0] }}</span>
+
+</template>
+<script>
+export default {
+  data() {
+    return {}
+  },
+  props: {
+    firstName: String,
+    lastName: String
+  },
+  computed: {
+    getRandomColor() {
+      const colors = [
+        "bg-green-500",
+        "bg-sky-500",
+        "bg-amber-400",
+        "bg-neutral-400",
+        "bg-orange-500",
+        "bg-violet-500",
+        "bg-rose-500",
+        "bg-lime-500",
+        "bg-emerald-500",
+        "bg-yellow-500",
+        "bg-teal-500",
+        "bg-cyan-500",
+        "bg-indigo-500",
+        "bg-fuchsia-500",
+        "bg-red-500"
+      ];
+      const randomIndex = Math.floor(Math.random() * colors.length);
+      return colors[randomIndex];
+    }
+  }
+  }
+
+</script>
--- webclient/pages/[organisationName]/settings/members.vue
+++ webclient/pages/[organisationName]/settings/members.vue
@@ -1,7 +1,8 @@
-<script setup>
+<script setup lang="ts">
 definePageMeta({
   middleware: ["auth", "route-check"]
 })
+
 </script>
 
 <template>
@@ -10,25 +11,24 @@ definePageMeta({
       <div class="mt-8 px-2 sm:px-[10%] lg:px-[15%]">
         <h1 class="text-2xl my-1 font-medium text-zinc-900">Members</h1>
         <p class="text-sm text-zinc-500 border-b border-zinc-300 pb-5">Manage the members within your workspace</p>
+        <div class="w-full py-4"><button class="float-right bg-zinc-50 hover:bg-zinc-100 drop-shadow-sm shadow-inner shadow-zinc-50 font-medium text-sm px-2.5 py-1 border border-zinc-300 rounded text-zinc-700"><i class="bi bi-person-plus-fill mr-1"></i>Invite</button></div>
         <div class="py-6">
-
           <div class="border border-zinc-300 rounded">
-            <table class="w-full text-sm text-left">
+            <table class="w-full text-xs text-left">
               <tr class="border-b border-zinc-300">
                 <td
-                  class="py-3 px-3 text-xs uppercase font-medium text-zinc-500 bg-zinc-100 border-zinc-300 rounded-tl">
+                  class="py-3 px-3 uppercase font-medium text-zinc-500 bg-zinc-100 border-zinc-300 rounded-tl">
                   NAME</td>
-                <td class="py-3 px-3 text-xs uppercase font-medium text-zinc-500 bg-zinc-100 border-zinc-300">EMAIL</td>
-                <td class="py-3 px-3 text-xs uppercase font-medium text-zinc-500 bg-zinc-100">ROLE</td>
-                <td v-show="userIsWorkspaceAdmin" class="py-3 px-3 text-xs uppercase font-medium text-zinc-500 bg-zinc-100 rounded-tr"></td>
+                <td class="py-3 px-3 uppercase font-medium text-zinc-500 bg-zinc-100 border-zinc-300">EMAIL</td>
+                <td class="py-3 px-3 uppercase font-medium text-zinc-500 bg-zinc-100">ROLE</td>
+                <td v-show="userIsWorkspaceAdmin" class="py-3 px-3 uppercase font-medium text-zinc-500 bg-zinc-100 rounded-tr"></td>
               </tr>
               <tr v-for="member in members" class="border-b border-zinc-300 last:border-0">
-                <td class="py-3 px-3 text-zinc-900"><span
-                    class="rounded-full bg-green-500 text-neutral-50 shadow-sm text-xs p-1.5 mr-2">{{ member.first_name[0] }}{{ member.last_name[0] }}</span>{{ member.first_name }}
+                <td class="py-3 px-3 text-zinc-900"><Avatar :first-name="member.first_name" :last-name="member.last_name"></Avatar>{{ member.first_name }}
                   {{ member.last_name }}</td>
                 <td class="py-3 px-3 text-zinc-500">{{ member.username }}</td>
                 <td class="py-3 px-3 text-zinc-500">{{ member.user_role }}</td>
-                <td v-show="userIsWorkspaceAdmin" class="py-3 px-3 text-zinc-500">
+                <td v-show="userIsWorkspaceAdmin" class="text-sm py-3 px-3 text-zinc-500">
                   <MemberListDropdown :username="member.username"></MemberListDropdown>
                 </td>
               </tr>
@@ -45,40 +45,47 @@ definePageMeta({
   </NuxtLayout>
 </template>
 
-<script>
+<script lang="ts">
+
+import { WorkspaceUser } from "~~/types/WorkspaceUser";
+
 export default {
   data() {
     return {
-      members: [
-        {
-          first_name: "Patrick",
-          last_name: "Schnyder",
-          username: "patrick@schnyder.com",
-          user_role: "Admin"
-        },
-        {
-          first_name: "Leopold",
-          last_name: "Pfeiffer",
-          username: "leopold@pfeiffer.com",
-          user_role: "Member"
-        },
-        {
-          first_name: "John",
-          last_name: "Doe",
-          username: "john@doe.com",
-          user_role: "Member"
-        },
-      ],
+      members: [],
       showError: false,
       userIsWorkspaceAdmin: false
-    };
+    }
   },
   async beforeMount() {
     //check if user is admin
     this.userIsWorkspaceAdmin = await useIsWorkspaceAdmin();
+    const user = useUserState();
+
+    const getUserWorkspace = await useFetchAuth(
+        'http://localhost:8000/workspace/users',{ method: 'GET',
+        params: {
+          workspace_id: user.value.workspaces[0]._id
+        }}
+        ).then((data:WorkspaceUser[]) => {
+          this.members = data;
+        }).catch((error) => {
+          console.log(error);
+          this.showError = true;
+          });
 
     
   },
-  methods: {}
+  computed: {
+    getRandomColor() {
+      const colors = [
+        "bg-green-400",
+        "bg-sky-400",
+        "bg-amber-400"
+      ];
+      const randomIndex = Math.floor(Math.random() * colors.length);
+      return colors[randomIndex];
+    }
+  }
 }
 </script>
