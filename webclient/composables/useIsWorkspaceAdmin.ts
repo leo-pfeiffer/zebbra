4add9de5cb7ea3e02889c696f13cc618824528ef
pschnyder <pat.schnyder96@gmail.com>
2022-06-30 20:41:20 +0100

    refactor: updated useIsWorkspaceAdmin to fit new datastructre

--- webclient/composables/useIsWorkspaceAdmin.ts
+++ webclient/composables/useIsWorkspaceAdmin.ts
@@ -5,14 +5,14 @@ import { GetWorkspaceResponse } from "~~/types/GetWorkspaceResponse";
 
 export const useIsWorkspaceAdmin = async () => {
 
-    var userUsername: String;
-    var adminUsername: String;
+    var userId: String;
+    var adminId: String;
 
     //get the username and store it
     const getUserUsername = await useFetchAuth(
         'http://localhost:8000/user',{ method: 'GET'}
         ).then((data:GetUserResponse) => {
-          userUsername = data.username;
+            userId = data._id;
 
         }).catch((error) => {
           console.log(error);
@@ -23,12 +23,12 @@ export const useIsWorkspaceAdmin = async () => {
     'http://localhost:8000/workspace',{ method: 'GET'}
     ).then((data:GetWorkspaceResponse) => {
         //get first workspace in response (user can only have one workspace)
-        adminUsername = data[0].admin;
+        adminId = data[0].admin;
     }).catch((error) => {
         console.log(error);
         });
 
     //return if equals
 
-    return (adminUsername === userUsername);
+    return (adminId === userId);
 }
