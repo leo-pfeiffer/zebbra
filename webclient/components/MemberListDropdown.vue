337d533fd25bce6e0d4a415d77cc8019fd7fb9e9
pschnyder <pat.schnyder96@gmail.com>
2022-07-03 15:55:20 +0100

    added teleport to modal

--- webclient/components/MemberListDropdown.vue
+++ webclient/components/MemberListDropdown.vue
@@ -16,47 +16,51 @@
     </div>
     <div v-show="clicked" @click="toggleDots" class="fixed top-0 left-0 w-[100vw] h-[100vh] z-0"></div>
 
-    <div v-show="deleteUserModalOpen" class="absolute -translate-x-full w-max h-max flex justify-center align-middle text-xs">
-        <div class="p-6 border h-max w-fit shadow-lg bg-white border-zinc-300 rounded">
-            <div>
-                <h3 class="text-zinc-900 font-medium text-sm mb-2">Do you want to remove this user?</h3>
-            </div>
-            <p class="text-zinc-500 mb-4">Removing <span class="font-semibold">{{ username }}</span> can't be undone.</p>
-            <div class="float-right">
-                <button
-                    class="bg-zinc-50 hover:bg-zinc-100 drop-shadow-sm shadow-inner shadow-zinc-50 font-medium text-xs px-2 py-1 border border-zinc-300 rounded text-zinc-700"
-                    @click="toggleDeleteUserModal">Cancel</button>
-                <button class="ml-2 bg-red-600  drop-shadow-sm
-                    shadow-zinc-50 text-xs font-medium px-2 py-1 
-                    border border-red-500 rounded text-neutral-100" @click="deleteUser">Remove</button>
-            </div>
-            <div v-show="showDeleteUserError" class="mt-11 flex justify-center">
-                <ErrorMessage :error-message="deleteUserErrorMessage"></ErrorMessage>
+    <Teleport to="body">
+        <div v-show="deleteUserModalOpen" class="fixed left-0 top-1/3 w-full h-full flex justify-center align-middle text-xs">
+            <div class="p-6 border h-max w-fit shadow-lg bg-white border-zinc-300 rounded">
+                <div>
+                    <h3 class="text-zinc-900 font-medium text-sm mb-2">Do you want to remove this user?</h3>
+                </div>
+                <p class="text-zinc-500 mb-4">Removing <span class="font-semibold">{{ username }}</span> can't be undone.</p>
+                <div class="float-right">
+                    <button
+                        class="bg-zinc-50 hover:bg-zinc-100 drop-shadow-sm shadow-inner shadow-zinc-50 font-medium text-xs px-2 py-1 border border-zinc-300 rounded text-zinc-700"
+                        @click="toggleDeleteUserModal">Cancel</button>
+                    <button class="ml-2 bg-red-600  drop-shadow-sm
+                        shadow-zinc-50 text-xs font-medium px-2 py-1 
+                        border border-red-500 rounded text-neutral-100" @click="deleteUser">Remove</button>
+                </div>
+                <div v-show="showDeleteUserError" class="mt-11 flex justify-center">
+                    <ErrorMessage :error-message="deleteUserErrorMessage"></ErrorMessage>
+                </div>
             </div>
         </div>
-    </div>
+    </Teleport>
 
-    <div v-show="changeAdminModalOpen" class="absolute -translate-x-full w-2/3 h-max flex justify-center align-middle text-xs">
-        <div class="p-6 border h-max shadow-lg bg-white border-zinc-300 rounded">
-            <div>
-                <h3 class="text-zinc-900 font-medium text-sm mb-2">Do you want to change the workspace admin?</h3>
-            </div>
-            <div class="flex align-middle mb-4">
-                <span class="text-zinc-500 mr-2 mt-1.5">Be aware that making <span class="font-semibold">{{ username }}</span> the admin of the workspace also removes you as an admin.</span>
-            </div>
-            <div class="float-right">
-                <button
-                    class="bg-zinc-50 hover:bg-zinc-100 drop-shadow-sm shadow-inner shadow-zinc-50 font-medium text-xs px-2 py-1 border border-zinc-300 rounded text-zinc-700"
-                    @click="toggleChangeAdminModal">Cancel</button>
-                <button @click="makeUserAdmin" class="ml-2 bg-sky-600  drop-shadow-sm
-                    shadow-zinc-50 text-xs font-medium px-2 py-1 
-                    border border-sky-500 rounded text-neutral-100">Change admin</button>
-            </div>
-            <div v-show="showChangeAdminError" class="mt-11 flex justify-center">
-                <ErrorMessage :error-message="changeAdminErrorMessage"></ErrorMessage>
+    <Teleport to="body">
+        <div v-show="changeAdminModalOpen" class="fixed left-0 top-1/3 w-full h-full flex justify-center align-middle text-xs">
+            <div class="p-6 border h-max shadow-lg bg-white border-zinc-300 rounded">
+                <div>
+                    <h3 class="text-zinc-900 font-medium text-sm mb-2">Do you want to change the workspace admin?</h3>
+                </div>
+                <div class="flex align-middle mb-4">
+                    <span class="text-zinc-500 mr-2 mt-1.5">Be aware that making <span class="font-semibold">{{ username }}</span> the admin of the workspace also removes you as an admin.</span>
+                </div>
+                <div class="float-right">
+                    <button
+                        class="bg-zinc-50 hover:bg-zinc-100 drop-shadow-sm shadow-inner shadow-zinc-50 font-medium text-xs px-2 py-1 border border-zinc-300 rounded text-zinc-700"
+                        @click="toggleChangeAdminModal">Cancel</button>
+                    <button @click="makeUserAdmin" class="ml-2 bg-sky-600  drop-shadow-sm
+                        shadow-zinc-50 text-xs font-medium px-2 py-1 
+                        border border-sky-500 rounded text-neutral-100">Change admin</button>
+                </div>
+                <div v-show="showChangeAdminError" class="mt-11 flex justify-center">
+                    <ErrorMessage :error-message="changeAdminErrorMessage"></ErrorMessage>
+                </div>
             </div>
         </div>
-    </div>
+    </Teleport>
 </div>
 </template>
 
