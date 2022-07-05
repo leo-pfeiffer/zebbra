bea41c923a9ff1a47ed75db43f3aca9a5ad20ad4
pschnyder <pat.schnyder96@gmail.com>
2022-07-05 19:07:45 +0100

    added page to test formula

---
+++ webclient/pages/formula.vue
@@ -0,0 +1,30 @@
+<template>
+    <div>
+        <form @submit.prevent="getValueObject">
+            <input required type="text" placeholder="input" v-model="input" class="border m-5">
+            <button type="submit">submit</button>
+        </form>
+        <div>
+            {{output}}
+        </div>
+    </div>
+</template>
+
+<script lang="ts">
+
+export default {
+    data() {
+        return {
+            input: "",
+            output: ""
+        }
+    },
+    methods: {
+        getValueObject() {
+
+            this.output = useFormulaParser().getValueObject(this.input);
+
+        }
+    }
+}
+</script>
