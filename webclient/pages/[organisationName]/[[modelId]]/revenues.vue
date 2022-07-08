a0130c03aa3947e513de0f7d472e3893b293ac87
pschnyder <pat.schnyder96@gmail.com>
2022-07-08 16:01:51 +0100

    feat: implemented adding new assumptions

--- webclient/pages/[organisationName]/[[modelId]]/revenues.vue
+++ webclient/pages/[organisationName]/[[modelId]]/revenues.vue
@@ -1,5 +1,5 @@
 <script setup lang="ts">
-import { Sheet, ModelMeta } from '~~/types/Model';
+import { Sheet, ModelMeta, Variable } from '~~/types/Model';
 
 definePageMeta({
     middleware: ["auth", "route-check"]
@@ -27,7 +27,7 @@ const assumptionValuesToDisplay = useState('assumptionValues');
     <NuxtLayout name="navbar">
         <div>
             <div class="mt-3 ml-1 py-3 pl-2 mr-0 overflow-x-hidden">
-                <div class="flex">
+                <div class="flex border-b border-zinc-300">
                     <div class="">
                         <div class="flex mb-4">
                             <div
@@ -49,6 +49,7 @@ const assumptionValuesToDisplay = useState('assumptionValues');
                         <AssumptionRow v-for="assumptionValues in assumptionValuesToDisplay" :values="assumptionValues"></AssumptionRow>
                     </div>
                 </div>
+                <div class="mt-2"><button class="bg-zinc-50 hover:bg-zinc-100 drop-shadow-sm shadow-inner shadow-zinc-50 font-medium text-xs pl-2.5 pr-3 py-1 border border-zinc-300 rounded text-zinc-700" @click="addAssumption">Add Assumption</button></div>
             </div>
         </div>
     </NuxtLayout>
@@ -60,7 +61,40 @@ export default {
         return {
         }
     },
-    methods: {},
+    methods: {
+        async addAssumption(){
+
+            const emptyAssumption:Variable = {
+
+                _id: undefined,
+                name: "New Assumption",
+                val_type: "number",
+                editable: true,
+                var_type: "value",
+                time_series: false,
+                starting_at: 0,
+                first_value_diff: false,
+                value: "0",
+                value_1: undefined, 
+                integration_values: undefined
+
+            }
+
+            const revenues = useRevenueState();
+            revenues.value.assumptions.push(emptyAssumption);
+            
+            const route = useRoute();
+
+            //todo: proper error handling
+            try {
+                await updateRevenueState(route.params.modelId, revenues.value);
+            } catch(e) {
+                console.log(e)
+                revenues.value = await getRevenueState(route.params.modelId)
+            }
+
+        }
+    },
     mounted() {
 
         //return the display values for all the assumptions
