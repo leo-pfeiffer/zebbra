7d0ab6b91eb910835914ac2a643699c2de5decde
leo-pfeiffer <leopold.pfeiffer@icloud.com>
2022-07-09 16:18:07 -0400

    chore: change logging level in prod to error

--- server/core/logger.py
+++ server/core/logger.py
@@ -1,4 +1,12 @@
 import logging
 
-logging.basicConfig(level=logging.INFO)
+from core.settings import get_settings
+
+settings = get_settings()
+
+if settings.ENV == "prod":
+    logging.basicConfig(level=logging.ERROR)
+else:
+    logging.basicConfig(level=logging.DEBUG)
+
 logger = logging.getLogger("root")
