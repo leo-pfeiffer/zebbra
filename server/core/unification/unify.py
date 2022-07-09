e37ba01877e73755d2fd5b7d34a669f26be09c72
leo-pfeiffer <leopold.pfeiffer@icloud.com>
2022-07-09 17:20:10 -0400

    feat: start implementation of unification process

--- server/core/unification/unify.py
+++ server/core/unification/unify.py
@@ -1,4 +1,11 @@
 import re
+from datetime import date
+
+from core.schemas.integrations import IntegrationProvider
+from core.schemas.rows import Row
+from core.schemas.sheets import Sheet
+from core.schemas.utils import DataBatch
+from core.unification.fetch import XeroFetchAdapter
 
 
 def parse_value(value_string: str) -> tuple[str, str]:
@@ -19,3 +26,40 @@ def parse_value(value_string: str) -> tuple[str, str]:
         return integration, endpoint
     except AssertionError:
         raise ValueError(f"Invalid value string {value_string}")
+
+
+def process_row(row: Row, data_batches: dict[IntegrationProvider, DataBatch]) -> Row:
+    """
+    Inplace modification
+    :param row:
+    :param data_batches:
+    :return:
+    """
+    if row.var_type != "integration":
+        return row
+
+    # catch error here?
+    integration, endpoint = parse_value(row.value)
+    # todo
+    ...
+    return row
+
+
+async def unify_data(sheet: Sheet, workspace_id: str, from_date: date):
+    adapter = XeroFetchAdapter(workspace_id)
+
+    data_batches: dict[IntegrationProvider, DataBatch] = {
+        "Xero": await adapter.get_data(from_date)
+    }
+
+    # assumptions
+    for row in sheet.assumptions:
+        process_row(row, data_batches)
+
+    # sections
+    for section in sheet.sections:
+        for row in section.rows:
+            process_row(row, data_batches=data_batches)
+        process_row(section.end_row, data_batches)
+
+    return Sheet
