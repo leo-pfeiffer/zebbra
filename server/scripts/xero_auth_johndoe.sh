6358e9186a1063161259607377c0e7d67224d068
leo-pfeiffer <leopold.pfeiffer@icloud.com>
2022-07-11 14:03:42 -0400

    fix: fix endpoint in xero auth script

--- server/scripts/xero_auth_johndoe.sh
+++ server/scripts/xero_auth_johndoe.sh
@@ -5,6 +5,6 @@
 
 ACCESS_TOKEN="eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJqb2huZG9lQGV4YW1wbGUuY29tIiwiZXhwIjoxNjU5MzgzNjkxfQ.yNkWCmM-3MGOydV_ysWb0cSeD2wqaJR-vpTOYdRpAlQ"
 WORKSPACE="62bc5706a40e85213c27ce29"
-URL="http://localhost:8000/api/integration/xero/login?workspace_id=${WORKSPACE}&access_token=${ACCESS_TOKEN}"
+URL="http://localhost:8000/integration/xero/login?workspace_id=${WORKSPACE}&access_token=${ACCESS_TOKEN}"
 
 open $URL
