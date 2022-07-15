4da77b77af82b17ad36e0a50ce42646868f919c9
pschnyder <pat.schnyder96@gmail.com>
2022-07-15 02:06:18 +0100

    bugfix!!(only semi): found bug that does not allow variable names that have nums... improved it that they cant only not start with numb

--- webclient/test/useGetValueFromHumanReadable.test.ts
+++ webclient/test/useGetValueFromHumanReadable.test.ts
@@ -46,12 +46,57 @@ describe('useGetValueFromHumanReadable Tests', () => {
         integration_values: undefined
     }
 
+    const varWithNumber:Variable = {
+        _id: "4",
+        name: "Customers 33",
+        val_type: "number",
+        editable: true,
+        var_type: "formula",
+        time_series: true,
+        starting_at: 0,
+        first_value_diff: true,
+        value: "$1*(1+#2)",
+        value_1: "#1",
+        integration_values: undefined
+    }
+
+    const varStartingWithNumber:Variable = {
+        _id: "5",
+        name: "2022 Customers",
+        val_type: "number",
+        editable: true,
+        var_type: "formula",
+        time_series: true,
+        starting_at: 0,
+        first_value_diff: true,
+        value: "$1*(1+#2)",
+        value_1: "#1",
+        integration_values: undefined
+    }
+
+    const otherVarStartingWithNumber:Variable = {
+        _id: "6",
+        name: "2022 Customer Growth",
+        val_type: "percentage",
+        editable: true,
+        var_type: "value",
+        time_series: false,
+        starting_at: 0,
+        first_value_diff: false,
+        value: "0.3",
+        value_1: undefined,
+        integration_values: undefined
+    }
+
     const variableSearchMap:Map<string, string> = new Map<string, string>();
 
     beforeAll(() => {
         variableSearchMap.set(initialCustomers._id, initialCustomers.name);
         variableSearchMap.set(customerGrowth._id, customerGrowth.name);
         variableSearchMap.set(customers._id, customers.name);
+        variableSearchMap.set(varWithNumber._id, varWithNumber.name);
+        variableSearchMap.set(varStartingWithNumber._id, varStartingWithNumber.name);
+        variableSearchMap.set(otherVarStartingWithNumber._id, otherVarStartingWithNumber.name);
     })
 
     it('should return the correct value from a human readable input', () => {
@@ -98,6 +143,33 @@ describe('useGetValueFromHumanReadable Tests', () => {
         const expectedOutput:string = "$1*(1+#2$1)"
 
         expect(useGetValueFromHumanReadable(humanReadableInput, customers._id, variableSearchMap)).toStrictEqual(expectedOutput);
+
+    })
+
+    it('should work with a number in the variable name', () => {
+
+        const humanReadableInput:string = "Customers 33[1] * (1 + Customer Growth[1])";
+        const expectedOutput:string = "$1*(1+#2$1)"
+
+        expect(useGetValueFromHumanReadable(humanReadableInput, varWithNumber._id, variableSearchMap)).toStrictEqual(expectedOutput);
+
+    })
+
+    it('should work when the variable starts with a number', () => {
+
+        const humanReadableInput:string = "2022 Customers[1] * (1 + Customer Growth[1])";
+        const expectedOutput:string = "$1*(1+#2$1)"
+
+        expect(useGetValueFromHumanReadable(humanReadableInput, varStartingWithNumber._id, variableSearchMap)).toStrictEqual(expectedOutput);
+
+    })
+
+    it('should work when the variable starts with a number', () => {
+
+        const humanReadableInput2:string = "2022 Customers[1] * (1 + 2022 Customer Growth[1])";
+        const expectedOutput2:string = "$1*(1+#6$1)"
+
+        expect(useGetValueFromHumanReadable(humanReadableInput2, otherVarStartingWithNumber._id, variableSearchMap)).toStrictEqual(expectedOutput2);
 
     })
 
