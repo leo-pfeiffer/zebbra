a6effacda14c0da744ee5a307945e7e3dc852b74
pschnyder <pat.schnyder96@gmail.com>
2022-07-25 23:46:37 +0100

    refactor: removed create new model button in settings

--- webclient/pages/[organisationName]/settings/models.vue
+++ webclient/pages/[organisationName]/settings/models.vue
@@ -12,9 +12,6 @@ const user = useUserState();
     <NuxtLayout name="settings-layout">
         <div class="container">
             <div class="mt-8 px-2 sm:px-[20%] lg:px-[30%]">
-                <button @click=""
-                    class="float-right mt-4 bg-zinc-50 hover:bg-zinc-100 drop-shadow-sm shadow-inner shadow-zinc-50 font-medium text-xs px-2 py-1 border border-zinc-300 rounded text-zinc-700"><i
-                        class="bi bi-plus-lg mr-1"></i>Create New Model</button>
                 <h1 class="text-2xl my-1 font-medium text-zinc-900">Your Models</h1>
                 <p class="text-sm text-zinc-500 border-b border-zinc-300 pb-5">Manage all your models here
                 </p>
