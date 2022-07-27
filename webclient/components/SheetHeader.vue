606d4cc9f24f14540f9537044b05d4b86d37ebd6
pschnyder <pat.schnyder96@gmail.com>
2022-07-28 00:23:41 +0100

    bugfix: color

--- webclient/components/SheetHeader.vue
+++ webclient/components/SheetHeader.vue
@@ -2,7 +2,7 @@
     <div class="relative">
         <span class="inline-block align-middle">
             <div class="">
-                <h1 class="font-semibold text-xl inline-block align-middle">{{sheetName}}</h1>
+                <h1 class="font-semibold text-xl inline-block align-middle text-zinc-900">{{sheetName}}</h1>
                 <p class="text-xs text-zinc-500">{{ workspaceName }} / {{ modelName }}
                 </p>
             </div>
