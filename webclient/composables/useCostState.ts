4c61d9dd1a86693c469df4b571dde57cf3698ad0
pschnyder <pat.schnyder96@gmail.com>
2022-07-29 11:40:18 +0100

    refactor: deleted unnecessary state composables

--- webclient/composables/useCostState.ts
+++
@@ -1,5 +0,0 @@
-import { Sheet } from "~~/types/Model"
-
-var costState: Sheet;
-
-export const useCostState = () => useState<Sheet>('costState', () => costState);
--- webclient/composables/useRevenueState.ts
+++
@@ -1,5 +0,0 @@
-import { Sheet } from "~~/types/Model"
-
-var revenueState: Sheet;
-
-export const useRevenueState = () => useState<Sheet>('revenueState', () => revenueState);
