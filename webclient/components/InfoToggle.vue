62c868c07c4a2e692564c1bc383989b3c21e78b4
pschnyder <pat.schnyder96@gmail.com>
2022-07-31 17:23:50 +0100

    bugfix: removed shadow

--- webclient/components/InfoToggle.vue
+++ webclient/components/InfoToggle.vue
@@ -1,5 +1,5 @@
 <template>
-    <span class="group right-3 z-50 max-w-fit shadow-sm" :class="{'absolute': position === 'absolute', 'ml-1 relative' : position === 'inline', 'relative' :position === 'list'}">
+    <span class="group right-3 z-50 max-w-fit" :class="{'absolute': position === 'absolute', 'ml-1 relative' : position === 'inline', 'relative' :position === 'list'}">
         <span class="absolute z-50 translate-y-7 -translate-x-1/4 text-center min-w-fit">
             <span
                 class="max-w-[350px] min-w-[200px] hidden z-50 font-normal bg-zinc-500 text-[11px] rounded px-2 py-1 text-white normal-case group-hover:inline-block">
